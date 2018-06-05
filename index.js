320de3eef0f761e182faa4157b7d70905424c1ec
B00031080 <B00031080@essec.edu>
2018-06-05 17:55:11 +0200

    Simple game

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const express = require('express');
-const app = express();
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/game", function (req, res) {
+    res.render('game', {});
+} );
+
+app.post("/gameresult", function (req, res) {
+    let numberUser1 = parseInt(req.body.numberUser1);
+    let numberUser2 = parseInt(req.body.numberUser2);
+    let numberComputer = parseInt(Math.random()*100);
+    let winningUser = (Math.abs(numberUser1 - numberComputer)<Math.abs(numberUser2 - numberComputer))?"User 1":"User 2";
+
+    res.render('gameresult', {numberUser1, numberUser2, numberComputer, winningUser});
+} );
+
+app.get('/', function (req, res) {
+    res.render('home', {maintenant: (new Date()).toLocaleTimeString(), students: [{name:"Marie","sex":"female"},{"name":"Joseph","sex":"male", honors:true},{"name":"Pierre","sex":"male"}]});
+});
+
+app.get('/produits/:category', function (req, res) {
+    let products = [];
+    if(req.params.category === "mobile"){
+        products = [{title:"iPhone X", description:"Cool and expensive"},{title:"Samsung S9", description:"Cool and less expensive"},{title:"OnePlus 6", description:"Cheap"}]
+    } else if(req.params.category === "cheese"){
+        products = [{title:"Compté", description:"Savoureux"},{title:"Emmental", description:"Fondant"},{title:"Camembert", description:"Odorant"}]
+    }
+
+    res.render('produit', {products:products});
+});
+
 app.get('/mapage', (req, res) => res.send('Tu ne devrais pas être là!!!'));
 app.use(express.static('client'));
 
